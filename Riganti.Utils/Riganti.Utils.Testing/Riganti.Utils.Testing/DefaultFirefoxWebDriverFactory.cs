3d7f2c7197ced3be0bd669ced692c303db3371c8
Ladislav Šesták <sestak@sense4code.com>
2016-01-05 11:53:56 +0100

    FirefoxWebDriver factory - checking of default install paths of firefox binary file

--- Riganti.Utils/Riganti.Utils.Testing/Riganti.Utils.Testing/DefaultFirefoxWebDriverFactory.cs
+++ Riganti.Utils/Riganti.Utils.Testing/Riganti.Utils.Testing/DefaultFirefoxWebDriverFactory.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
@@ -7,7 +9,31 @@ namespace Riganti.Utils.Testing.SeleniumCore
     {
         public IWebDriver CreateNewInstance()
         {
-            return new FirefoxDriver();
+            try
+            {
+                return new FirefoxDriver();
+            }
+            catch (Exception exception)
+            {
+                var env = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                var firefox = "Mozilla Firefox\\Firefox.exe";
+                if (File.Exists(Path.Combine(env, firefox)))
+                {
+                    return AlternativeInstance(env, firefox);
+                }
+
+                env = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+                if (File.Exists(Path.Combine(env, firefox)))
+                {
+                    return AlternativeInstance(env, firefox);
+                }
+                throw;
+            }
+        }
+
+        private static IWebDriver AlternativeInstance(string env, string firefox)
+        {
+            return new FirefoxDriver(new FirefoxBinary(Path.Combine(env, firefox)), null);
         }
     }
 }
