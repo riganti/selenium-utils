4b7f7e2842a79e24b9ed7a2ba74cba224b8487fe
Ladislav Šesták <sestak@sense4code.com>
2016-02-15 16:11:37 +0100

    Chrome incognito

--- Riganti.Utils/Riganti.Utils.Testing/Riganti.Utils.Testing/DefaultChromeWebDriverFactory.cs
+++ Riganti.Utils/Riganti.Utils.Testing/Riganti.Utils.Testing/DefaultChromeWebDriverFactory.cs
@@ -1,6 +1,5 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
 
 namespace Riganti.Utils.Testing.SeleniumCore
 {
@@ -10,8 +9,8 @@ namespace Riganti.Utils.Testing.SeleniumCore
         {
             var options = new ChromeOptions();
             options.AddArgument("test-type");
+            options.AddArgument("-incognito");
             var driver = new ChromeDriver(options);
-
             driver.SetDefaultTimeouts();
             return driver;
         }
