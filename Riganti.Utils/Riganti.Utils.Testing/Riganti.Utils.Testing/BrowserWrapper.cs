d866d9d89f037ad2da9b86b4edb3bfa65abb7b95
MichalTichy <reg@tichymichal.net>
2016-04-27 21:36:53 +0200

    Edited WaitFor

--- Riganti.Utils/Riganti.Utils.Testing/Riganti.Utils.Testing/BrowserWrapper.cs
+++ Riganti.Utils/Riganti.Utils.Testing/Riganti.Utils.Testing/BrowserWrapper.cs
@@ -684,17 +684,17 @@ namespace Riganti.Utils.Testing.SeleniumCore
         /// <param name="condition">Expression that determine whether test should wait or continue</param>
         /// <param name="maxTimeout">If condition is not reached in this timeout (ms) test is dropped.</param>
         /// <param name="failureMessage">Message which is displayed in exception log in case that the condition is not reached</param>
-        /// <param name="ignoreCertainException">When StaleElementReferenceException or InvalidElementStateException is thrown, then it would be ignored.</param>
+        /// <param name="ignoreCertainException">When StaleElementReferenceException or InvalidElementStateException is thrown than it would be ignored.</param>
         /// <returns></returns>
         public BrowserWrapper WaitFor(Func<bool> condition, int maxTimeout, string failureMessage, bool ignoreCertainException = true)
         {
+            if (condition == null)
+            {
+                throw new NullReferenceException("Condition cannot be null.");
+            }
+            var now = DateTime.UtcNow;
             try
             {
-                if (condition == null)
-                {
-                    throw new NullReferenceException("Condition cannot be null.");
-                }
-                var now = DateTime.UtcNow;
                 while (!condition())
                 {
                     if (DateTime.UtcNow.Subtract(now).TotalMilliseconds > maxTimeout)
