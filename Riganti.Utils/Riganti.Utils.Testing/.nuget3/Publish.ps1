e075453fd2417a5e864befe490b0e09d60dfdee1
Ladislav Šesták <ladislav.sestak@riganti.cz>
2017-06-08 00:28:21 +0200

    Publish script updated

--- Riganti.Utils/Riganti.Utils.Testing/.nuget3/Publish.ps1
+++ Riganti.Utils/Riganti.Utils.Testing/.nuget3/Publish.ps1
@@ -1,10 +1,10 @@
 param([String]$key)
 
-del ./Riganti.Utils.Testing.Selenium.Core.*.nupkg
+del ./Riganti.Utils.Testing.Selenium.*.nupkg
 
 & .\NuGet.exe pack "./Riganti.Utils.Testing.Selenium.Dotvvm.nuspec"
 
-$file = dir ./Riganti.Utils.Testing.Selenium.Dotvvm.*.nupkg
+$file = dir ./Riganti.Utils.Testing.Selenium.*.nupkg
 $file = $file.Name
 
-& .\NuGet.exe push $file $key
+& .\NuGet.exe push $file -apikey $key  -source "https://api.nuget.org/v3/index.json" 
