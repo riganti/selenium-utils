d92a75896863afabf8733b3838905f3cc08069ff
Rumbeer <433545@mail.muni.cz>
2017-09-25 18:59:08 +0200

    Fixed and revised FunctionUITests (with the exception of cookie tests)

--- Riganti.Utils/Samples/Riganti.Utils.Testing.Selenium.Core.Samples.New/Controllers/TestController.cs
+++ Riganti.Utils/Samples/Riganti.Utils.Testing.Selenium.Core.Samples.New/Controllers/TestController.cs
@@ -9,6 +9,10 @@ namespace Riganti.Utils.Testing.Selenium.Core.Samples.New.Controllers
     public class TestController : Controller
     {
         public ActionResult Alert() { return View(null); }
+        public ActionResult Attribute()
+        {
+            return View(null);
+        }
         public ActionResult Checkboxes() { return View(null); }
         public ActionResult ClickTest() { return View(null); }
         public ActionResult Confirm() { return View(null); }
@@ -23,6 +27,10 @@ namespace Riganti.Utils.Testing.Selenium.Core.Samples.New.Controllers
             ControllerContext.HttpContext.Response.Cookies.Append("test_cookie", "cookie set to this value");
             return View("CookiesTest", new CookieModel() { Text = ControllerContext.HttpContext.Request.Cookies["test_cookie"] });
         }
+        public ActionResult Displayed()
+        {
+            return View(null);
+        }
         public ActionResult ElementAtFirst() { return View(null); }
         public ActionResult ElementContained() { return View(null); }
         public ActionResult ElementSelectionTest() { return View(null); }
@@ -30,9 +38,10 @@ namespace Riganti.Utils.Testing.Selenium.Core.Samples.New.Controllers
         public ActionResult FileDialog() { return View(null); }
         public ActionResult FrameTest1() { return View(null); }
         public ActionResult FrameTest2() { return View(null); }
+        public ActionResult FindElements() { return View(null); }
         public ActionResult HyperLink() { return View(null); }
         public ActionResult Index() { return View(null); }
-        public ActionResult JsHtmlTest() { return View(null); }
+        public ActionResult JsView() { return View(null); }
         public ActionResult JSPropertySetTest() { return View(null); }
         public ActionResult JSTestSite() { return View(null); }
         public ActionResult NoParentTest() { return View(null); }
@@ -40,6 +49,7 @@ namespace Riganti.Utils.Testing.Selenium.Core.Samples.New.Controllers
         public ActionResult Site2() { return View(null); }
         public ActionResult TemporarySelector() { return View(null); }
         public ActionResult Text() { return View(null); }
+        public ActionResult Value() { return View(null); }
 
     }
 
