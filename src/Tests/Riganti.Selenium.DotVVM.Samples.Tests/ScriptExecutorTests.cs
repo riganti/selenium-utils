718541bd063b9cdb7947058b87c6f37ade496269
Ladislav Schumacher <ladislav.schumacher@riganti.cz>
2017-11-16 14:24:40 +0100

    add script executor tests

---
+++ src/Tests/Riganti.Selenium.DotVVM.Samples.Tests/ScriptExecutorTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.DotVVM.Samples.Tests
+{
+    [TestClass]
+    public class ScriptExecutorTests : AppSeleniumTest
+    {
+        [TestMethod]
+        public void RunScriptExecutor()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl();
+
+                browser.GetJavaScriptExecutor().ExecuteScript("$('#WaitButton').click()");
+
+                browser.WaitFor(() =>
+                {
+                    browser.First("#LabelText").CheckIfInnerTextEquals("PostbackEnd");
+                }, 5000);
+            });
+        }
+    }
+}
