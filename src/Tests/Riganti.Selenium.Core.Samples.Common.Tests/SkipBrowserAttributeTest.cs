b164bba1d4c9d10d9a3c2bffa1e2b403306eb5a7
Adam Jež <adamjez@outlook.cz>
2018-07-20 09:55:04 +0200

    Added AppSeleniumTest base class

--- src/Tests/Riganti.Selenium.Core.Samples.Common.Tests/SkipBrowserAttributeTest.cs
+++ src/Tests/Riganti.Selenium.Core.Samples.Common.Tests/SkipBrowserAttributeTest.cs
@@ -1,29 +1,21 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
-using Riganti.Selenium.AssertApi;
-using Riganti.Selenium.Core.Abstractions;
 using Riganti.Selenium.Core.Abstractions.Attributes;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Riganti.Selenium.Core.Samples.AssertApi.Tests
 {
-    public class SkipBrowserAttributeTest : SeleniumTest
+    public class SkipBrowserAttributeTest : AppSeleniumTest
     {
         public SkipBrowserAttributeTest(ITestOutputHelper output) : base(output)
         {
         }
 
         [Fact]
-        [SkipBrowser("chrome:fast", "Skips this test to prove this attribute works")]
+        [SkipBrowser("chrome:fast", "Skip this test to prove this attribute works")]
         public void ThrowExceptionInTest_ExpectedSkippedTest()
         {
             RunInAllBrowsers(_ => throw new Exception("This exception will be skipped"));
-        }
-
-        private void RunInAllBrowsers(Action<IBrowserWrapper> testBody, [CallerMemberName]string callerMemberName = "", [CallerFilePath]string callerFilePath = "", [CallerLineNumber]int callerLineNumber = 0)
-        {
-            AssertApiSeleniumTestExecutorExtensions.RunInAllBrowsers(this, testBody, callerMemberName, callerFilePath, callerLineNumber);
         }
     }
 }
