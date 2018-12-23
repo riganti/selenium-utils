fcb0499e3acd2b5725ce9cb45579d595e814f337
Ladislav Šesták <ladislav.sestak@riganti.cz>
2018-12-23 14:58:38 +0100

    Alert api stability test

---
+++ src/Tests/Riganti.Selenium.Core.Samples.Assert.Tests/AlertTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Riganti.Selenium.Core.Samples.AssertApi.Tests
+{
+    public class AlertTests : AppSeleniumTest
+    {
+        public AlertTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void AlertApiStabilityTest()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl("/test/Alert");
+                var button = browser.First("input[type=button]");
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+
+                browser.NavigateToUrl("/test/Confirm");
+                var message = browser.First("#message");
+                button = browser.First("input[type=button]");
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.ConfirmAlert();
+                AssertUI.TextEquals(message, "Accept");
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+                AssertUI.TextEquals(message, "Dismiss");
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.ConfirmAlert();
+                AssertUI.TextEquals(message, "Accept");
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.DismissAlert();
+                AssertUI.TextEquals(message, "Dismiss");
+
+                button.Click();
+                browser.Wait();
+                AssertUI.AlertText(browser, s => s == "confirm test");
+                browser.ConfirmAlert();
+                AssertUI.TextEquals(message, "Accept");
+            });
+        }
+    }
+}
