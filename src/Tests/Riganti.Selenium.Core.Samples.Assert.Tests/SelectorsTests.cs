5af0f0e0aa342a4028022b3e50f6ba752ed61566
Ladislav Šesták <sestakladislav.jr@gmail.com>
2021-05-05 16:17:34 +0200

    Fixed collection selectorMethod references

---
+++ src/Tests/Riganti.Selenium.Core.Samples.Assert.Tests/SelectorsTests.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using Riganti.Selenium.AssertApi;
+using System.Security.Principal;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Riganti.Selenium.Core.Samples.AssertApi.Tests
+{
+
+    public class SelectorsTests : AppSeleniumTest
+    {
+        public SelectorsTests(ITestOutputHelper output) : base(output)
+        {
+
+        }
+        public By SelectByDataUi(string selector)
+            => SelectBy.CssSelector($"[data-ui='{selector}']");
+
+        [Fact]
+        public void SelectByCssSelectorTest()
+        {
+            RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl("/test/Index");
+
+                var div = browser.Single("selector-test", SelectByDataUi);
+                var span = div.Single("span");
+            });
+        }
+
+
+    }
+
+
+}
