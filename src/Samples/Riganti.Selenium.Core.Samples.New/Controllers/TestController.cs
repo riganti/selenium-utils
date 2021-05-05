f442531069099ca23934c59a2803329d267d77da
Ladislav Šesták <sestakladislav.jr@gmail.com>
2021-05-05 11:38:45 +0200

    Added test for scrolling

--- src/Samples/Riganti.Selenium.Core.Samples.New/Controllers/TestController.cs
+++ src/Samples/Riganti.Selenium.Core.Samples.New/Controllers/TestController.cs
@@ -49,6 +49,7 @@ namespace Riganti.Selenium.Core.Samples.New.Controllers
         public ActionResult Index() { return View(null); }
         public ActionResult Focus() { return View(null); }
         public ActionResult JsView() { return View(null); }
+        public ActionResult ScrollTo() { return View(null); }
         public ActionResult SelectMethod() { return View(null); }
         public ActionResult TemporarySelector() { return View(null); }
         public ActionResult Text() { return View(null); }
