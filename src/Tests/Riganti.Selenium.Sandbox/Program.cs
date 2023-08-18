5331bb672568d7ced6c9654a54bdd59742d2eaec
Adam Štěpánek <adam.stepanek@riganti.cz>
2023-08-18 15:48:27 +0200

    Add another test to Sandbox

--- src/Tests/Riganti.Selenium.Sandbox/Program.cs
+++ src/Tests/Riganti.Selenium.Sandbox/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using Riganti.Selenium.Core;
 
 namespace Riganti.Selenium.Sandbox;
@@ -8,6 +9,7 @@ public class Program : SeleniumTest
     public static void Main()
     {
         var test = new Program();
+
         test.RunInAllBrowsers(browser =>
         {
             browser.NavigateToUrl("/test/CssStyles");
@@ -20,5 +22,18 @@ public class Program : SeleniumTest
                 browser.First("#hasNotStyles")
                     .CheckCssStyle("margin-left", "8px");
         });
+        
+        test.RunInAllBrowsers(browser =>
+            {
+                browser.NavigateToUrl("/test/FindElements");
+                var a = browser.FindElements("div");
+                var b = a.FindElements("p");
+                var c = b.BrowserWrapper;
+                var d = c.FindElements("div");
+                var e = d.FindElements("p");
+                e.ThrowIfSequenceEmpty();
+            });
+        
+        Console.WriteLine("Exiting successfully...");
     }
 }
