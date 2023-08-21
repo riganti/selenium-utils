904ff13bdb151419afef4ad14c5bfcd3cb99551a
Adam Štěpánek <adam.stepanek@riganti.cz>
2023-08-21 16:29:32 +0200

    Ignore NoAlertPresentExceptions in FastWebBrowserBase

--- src/Core/Riganti.Selenium.Core/Drivers/FastWebBrowserBase.cs
+++ src/Core/Riganti.Selenium.Core/Drivers/FastWebBrowserBase.cs
@@ -112,7 +112,14 @@ namespace Riganti.Selenium.Core.Drivers
         /// </summary>
         protected virtual void DismissAllAlerts()
         {
-            driverInstance.SwitchTo()?.Alert()?.Dismiss();
+            try
+            {
+                driverInstance.SwitchTo()?.Alert()?.Dismiss();
+            }
+            catch (NoAlertPresentException)
+            {
+                // ignore it
+            }
         }
 
         /// <summary>
@@ -125,7 +132,7 @@ namespace Riganti.Selenium.Core.Drivers
             if (driverWithExecutor is null) return;
 
             driverWithExecutor.CommandExecutor.Dispose();
-            var s= Process.Start("");
+            var s = Process.Start("");
         }
     }
 }
