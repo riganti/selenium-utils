1be25932ee280b4b1c9715fb063ada6fb64e20b7
Ladislav Šesták <quigamdev@users.noreply.github.com>
2024-09-07 16:50:55 +0100

    Update src/Core/Riganti.Selenium.Core/BrowserWrapper.cs
    Co-authored-by: Standa Lukeš <exyi@outlook.cz>

--- src/Core/Riganti.Selenium.Core/BrowserWrapper.cs
+++ src/Core/Riganti.Selenium.Core/BrowserWrapper.cs
@@ -89,7 +89,7 @@ namespace Riganti.Selenium.Core
         /// <summary>
         /// Gives path of url of active browser tab.
         /// </summary>
-        public string CurrentUrlPath => new Uri(CurrentUrl).GetComponents(UriComponents.PathAndQuery | UriComponents.Fragment & ~UriComponents.HostAndPort, UriFormat.UriEscaped);
+        public string CurrentUrlPath => new Uri(CurrentUrl).GetComponents(UriComponents.PathAndQuery | UriComponents.Fragment, UriFormat.UriEscaped);
 
 
 
